24f1bec0d1f32b08ef51d55a14b6f309222d3558
Emily Behlmann <ebehlmann@gmail.com>
2014-08-16 20:07:28 -0500

    Fixed spacing issue

--- js/scripts.js
+++ js/scripts.js
@@ -36,13 +36,13 @@ Builds a new version of the array by picking a random element to include first,
 excluding it, then picking again, etc.
 */
 var shuffleArray = function(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
 }
 
 
